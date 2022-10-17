db2c8ea21468f92bd5aa23e5cb0b0b7e8425ae04
Michael Newlon <michael.newlon@tn.gov>
2022-10-17 10:09:14 -0400

    i3 | generate keys

--- src/controllers/generate_controller.js
+++ src/controllers/generate_controller.js
@@ -34,18 +34,45 @@ function routeGetToken(req, res) {
     }
 }
 
+function checkRequiredUserFields(req) {
+    var response = { message: '' }
+    if (req.body.username === undefined) 
+        response.message = 'Username expected but not found.'
+    
+    if (req.body.email === undefined) 
+        response.message = 'Email expected but not found.'
+    
+    return response
+}
+
+
 function routePostUserKey(req, res) {
-    var response = routeGetKey(req, res)
-    response.data.username = req.body.username
-    response.data.email = req.body.email
+    var response = {}
+    if (checkRequiredUserFields().message !== '') 
+        response.status = 400
+    else {
+        response = routeGetKey(req, res)
+        if (response.status === 200) {
+            response.data.username = req.body.username
+            response.data.email = req.body.email
+        }
+    }
     return response
 }
 
 // eslint-disable-next-line no-unused-vars
 function routePostUserToken(req, res) {
-    var response = routeGetToken(req, res)
-    response.data.username = req.body.username
-    response.data.email = req.body.email
+    var response = {}
+    if (checkRequiredUserFields().message !== '') 
+        response.status = 400
+    else {
+        response = routeGetToken(req, res)
+
+        if (response.status === 200) {
+            response.data.username = req.body.username
+            response.data.email = req.body.email
+        }
+    }
     return response
 }
 
